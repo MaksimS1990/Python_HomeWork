132ac15a1326dbe6f2c1a09937c8cd4051021a61
MaksimS <sipatovGT@mail.ru>
2023-01-17 19:10:26 +0400

    task41needtocomplet

--- Python_DZ6/task41.py
+++ Python_DZ6/task41.py
@@ -3,27 +3,37 @@
 
 fileName = 'tel.txt'
 
-def writeFile(fileName):                            # функция записи в файл
+def writeFile(fileName):                               # функция записи в файл
     with open (fileName, 'a') as data:
         data.writelines('\n' + input("Введите данные: ") + '\n')
     
-def readFile(fileName):                             # функция чтения из файла
-    result = []                                     # создаем пустой список
+def readFile(fileName):                                # функция чтения из файла
+    result = []                                        # создаем пустой список
     with open (fileName, 'r+') as data:
-        for line in data:                           # бежим по списку списков
-            result.append(line.split())             # преобразуем строку в список
+        for line in data:                              # бежим по списку списков
+            result.append(line.split())                # преобразуем строку в список
         return result
 
-def findUsers(userlist):                            # ищем по индексу (Имя -> номер телефона)
-    name = input("Введите имя: ")
-    for user in userlist:
-        if user[1] == name:
-            print(user[3])
+#def findUsers(userlist):                              # ищем по индексу (Имя -> номер телефона)
+    #name = input("Введите имя: ")
+    #for user in userlist:
+        #if user[1] == name:
+            #print(user[3])
 
 def DeleteUsers(userlist):
-    userlist.remove(input("Введите данные: "))
+    cancel = input("Введите данные: ")
+    for user in userlist:
+        if cancel in user:
+            userlist.remove(user)
+            print(userlist)
 
-writeFile(fileName)
-print(readFile(fileName))
-findUsers(readFile(fileName))
-DeleteUsers(userlist)
+def overwritingFile(userlist):
+    with open (fileName, 'w') as data:
+        data.writelines("'\n' userlist '\n'")
+print(fileName)
+
+# writeFile(fileName)
+# print(readFile(fileName))
+# findUsers(readFile(fileName))
+DeleteUsers(readFile(fileName))
+overwritingFile(readFile(fileName))
