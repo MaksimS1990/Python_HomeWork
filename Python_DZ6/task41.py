adfecbe294aa7a49ebdb356c6851528d5cc7f33b
MaksimS <sipatovGT@mail.ru>
2023-01-20 15:10:20 +0400

    task41needtoadded

--- Python_DZ6/task41.py
+++ Python_DZ6/task41.py
@@ -1,46 +1,56 @@
 fileName = 'tel.txt'
 
-def writeFile(fileName):                               # функция записи в файл
+def ConvertToString(listUser):
+    stringUser = ''
+    for i in range(0, len(listUser)-1):
+        stringUser = stringUser + listUser[i] + ', '
+    stringUser = stringUser + listUser[-1]
+    return stringUser
+
+def ConvertToList(stringUser):
+    stringUser = stringUser.replace('\n','')
+    return list(stringUser.split(', '))
+
+def writeFile(fileName):
     with open (fileName, 'a') as data:
-        data.writelines('\n' + input("Введите данные контакта: ") + '\n')
-    
-def readFile(fileName):                                # функция чтения из файла
-    result = []                                        # создаем пустой список
-    with open (fileName, 'r+') as data:
-        for line in data:                              # бежим по списку списков
-            result.append(line.split())                # преобразуем строку в список
+        data.writelines(input("Введите данные нового контакта: ") + '\n')
+
+def readFile(fileName):                                          # функция чтения из файла 
+    result = []                                                  # создаем пустой список 
+    with open (fileName, 'r+') as data: 
+        for line in data:                                        # бежим по списку списков 
+            result.append(ConvertToList(line))                   # преобразуем строку в список
         return result
 
-def findUsers(userlist):                              # ищем по индексу (Имя -> данные контакта)
-    cancel = input("Введите Ф/И/0/ номер: ")
-    for user in userlist:
-        if cancel in user:
-            print(user)
-    return userlist
+def findUsers(result):                                           # ищем по индексу (Имя -> данные контакта)
+    cancel = input("Введите Ф/И/0/ или номер искомого контакта: ")
+    for user in result:
+            if cancel in user:
+                print(user)
+    return user
+    
+def DeleteUsers(fileName):                                       # удаляем контакт по Ф/И/О либо номеру телефона
+    cancel = int(input("Введите номер контакта для удаления: "))
+    fileName.pop(cancel-1)                                       # remove - функция удаления списка
+    print(fileName)
+    return fileName
 
-# def PhoneNumberReplacement(userlist):
-    # cancel = input("Введите Ф/И/0/ номер: ")
-    # for user in userlist:
-        # if cancel in user:
-            # cancel[3] =  input("Введите новый номер телефона: ")
-    # return userlist
+def writingNewNumber(result):
+    cancel = input("Введите Ф/И/0/ или номер искомого контакта для смены номера: ")
+    for user in result:
+            if cancel in user:
+                user[3] = (input("Введите новый номер: "))
+                print(user)
+                print(result)
+    return result
 
-def DeleteUsers(userlist):                            # удаляем контакт по Ф/И/О либо номеру телефона
-    cancel = input("Введите данные контакта для удаления: ")
-    for user in userlist:
-        if cancel in user:
-            userlist.remove(user)                     # remove - функция удаления списка
-            print(userlist)
-    return userlist
+def fileOverwritting(fileName):
+     with open (fileName, 'w') as data:
+         data.writelines(result + '\n')
 
-def overwritingFile(userlist):
-    with open (fileName, 'w') as data:
-        for user in userlist:
-            data.writelines(f"\n {user}")
-print(fileName)
-
-writeFile(fileName)
-print(readFile(fileName))
-findUsers(readFile(fileName))
-PhoneNumberReplacement(readFile(fileName))
-overwritingFile(DeleteUsers(readFile(fileName)))
+# writeFile(fileName)
+# print(readFile(fileName))
+# print(findUsers(readFile(fileName)))
+# DeleteUsers(readFile(fileName))
+writingNewNumber(readFile(fileName))
+fileOverwritting(readFile(fileName))
